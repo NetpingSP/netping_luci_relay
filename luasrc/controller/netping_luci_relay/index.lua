389f0611a996f77e827df8c90f1891c5a833ce19
Anton Gavrilyuk <amigoalt@gmail.com>
2020-10-28 02:42:03 +0300

    LuCI JS Client API was applied for rendering "protocols" dropdown and modal window.

--- luasrc/controller/netping_luci_relay/index.lua
+++ luasrc/controller/netping_luci_relay/index.lua
@@ -3,6 +3,8 @@ module("luci.controller.netping_luci_relay.index", package.seeall)
 local config = "settings"
 local http = require "luci.http"
 local uci = require "luci.model.uci".cursor()
+local util = require "luci.util"
+
 
 function index()
 	if nixio.fs.access("/etc/config/settings") then
@@ -11,22 +13,25 @@ function index()
 	end
 end
 
-function do_action(action, payload, relay)
+function do_action(action, relay)
+	local payload = luci.http.formvalue()
+	local allowed_options = util.keys(uci:get_all(config, "prototype"))
 	local commands = {
 		add = function(...)
-			local default_name, count, record = "Relay", 1, {}
+			local default_name = uci:get(config, "globals", "default_name")
+			local count, record = 1, {}
 			uci:foreach(config, "relay", function() count = count + 1 end)
 			record = {
-				["name"] = default_name .. " " .. count,
-				["state"] = "0",
-				["embedded"] = "0",
+				["name"] = default_name .. " " .. count
 			}
 			uci:section(config, "relay", nil, record)
 			uci:commit(config)
 		end,
-		rename = function(relay, new_name)
-			uci:set(config, relay, "name", new_name)
-			uci:commit(config)
+		rename = function(relay, payload)
+			if payload["name"] then
+				uci:set(config, relay, "name", payload["name"])
+				uci:commit(config)
+			end
 		end,
 		delete = function(relay, ...)
 			-- protect embedded relays from deleting
@@ -41,6 +46,14 @@ function do_action(action, payload, relay)
 			local new_state = (old_state + 1) % 2
 			uci:set(config, relay, "state", new_state)
 			uci:commit(config)
+		end,
+		edit = function(relay, payload)
+			for key, value in payload do
+				if util.contains(allowed_payloads, key) then
+					uci:set(config, relay, key, value)
+				end
+				uci:commit(config)
+			end
 		end,
 		default = function(...)
 			http.prepare_content("text/plain")
--- netping_luci_relay/luasrc/model/cbi/netping_luci_relay/relay.lua
+++
@@ -1,8 +0,0 @@
-
-local config, title = "settings", "Settings"
-
-m = Map(config, title)
-m.template = "netping_luci_relay/table"
-m.pageaction = false
-
-return m
